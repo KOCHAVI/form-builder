ea7183e40b5f40d22852fb0c7aa3eaa1ae67af65
KOCHAVI <roiko1312@gmail.com>
2025-07-02 22:59:23 +0300

    nodemon and logger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 
+import loggerMiddleware from './middlewares/logger';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(loggerMiddleware);
 
 app.get('/health', (req, res) => {
     res.sendStatus(200);
